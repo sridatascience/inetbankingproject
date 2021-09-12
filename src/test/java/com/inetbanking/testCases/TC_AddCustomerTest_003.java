6784f533a925e6634918b488fcef29f7327e6aca
sridharvumma <sridhar6358@gmail.com>
2021-09-12 20:20:44 +0530

    Inet banking project

---
+++ src/test/java/com/inetbanking/testCases/TC_AddCustomerTest_003.java
@@ -0,0 +1,70 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.AddCustomerPage;
+import com.inetbanking.pageObjects.LoginPage;
+
+public class TC_AddCustomerTest_003 extends BaseClass
+{
+
+	@Test
+	public void addNewCustomer() throws InterruptedException, IOException
+	{
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("User name is provided");
+		lp.setPassword(password);
+		logger.info("Passsword is provided");
+		lp.clickSubmit();
+		
+		Thread.sleep(3000);
+		
+		AddCustomerPage addcust=new AddCustomerPage(driver);
+		
+		addcust.clickAddNewCustomer();
+		
+		logger.info("providing customer details....");
+		
+		
+		addcust.custName("Sridhar");
+		addcust.custgender("male");
+		addcust.custdob("02","13","1982");
+		Thread.sleep(5000);
+		addcust.custaddress("INDIA");
+		addcust.custcity("BANG");
+		addcust.custstate("KA");
+		addcust.custpinno("560043");
+		addcust.custtelephoneno("9591729335");
+		
+		String email=randomestring()+"@gmail.com";
+		addcust.custemailid(email);
+		addcust.custpassword("123456");
+		addcust.custsubmit();
+		
+		Thread.sleep(3000);
+		
+		logger.info("validation started....");
+		
+		boolean res=driver.getPageSource().contains("Customer Registered Successfully!!!");
+		
+		if(res==true)
+		{
+			Assert.assertTrue(true);
+			logger.info("test case passed....");
+			
+		}
+		else
+		{
+			logger.info("test case failed....");
+			captureScreen(driver,"addNewCustomer");
+			Assert.assertTrue(false);
+		}
+			
+	}
+	
+	
+}
